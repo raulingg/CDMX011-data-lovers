d4abecccaa4504f5125b04901d69ca2485653da1
Victoria Tejeda <vicyatm@gmail.com>
2021-07-27 23:03:16 -0500

    Agregamos diseño css

--- src/data.js
+++ src/data.js
@@ -1,39 +1,62 @@
 // estas funciones son de ejemplo
-
 //import athletes from "./data/athletes/athletes.js";
-import data from './data/athletes/athletes.js';
+import data from "./data/athletes/athletes.js";
 
-export const name = data.athletes.sort((a,b) => {
-  return (a.name < b.name) ? - 1 : 1
-})
- //console.log(name);
+export const name = data.athletes.sort((a, b) => {
+  return a.name < b.name ? -1 : 1;
+});
+//console.log(name);
 
-/*export const sport = data.athletes.filter(function(spt) {
-  return spt.sport == "Volleyball";
-});*/
-
-export function searchByTeam(pais){
-  let filterPerTeam=data.athletes.filter(function(team) {
-  return team.team == pais;
-})
-return filterPerTeam
+export function searchByTeam(dataX, pais, buscarTodo) {
+  let filterPerTeam;
+  if (buscarTodo) {
+    filterPerTeam = data.athletes.filter(function (atleta) {
+      return atleta.team == pais;
+    });
+  } else {
+    filterPerTeam = dataX.filter(function (atleta) {
+      return atleta.team == pais;
+    });
+  }
+  return filterPerTeam;
 }
-export function searchBySport(Deportes){
-  let filterPerSport=data.athletes.filter(function(sport) {
-  return sport.sport == Deportes;
-})
-return filterPerSport
+export function searchBySport(dataX, deporte, buscarTodo) {
+  let filterPerSport;
+  if (buscarTodo) {
+    filterPerSport = data.athletes.filter(function (atleta) {
+      return atleta.sport == deporte;
+    });
+  } else {
+    filterPerSport = dataX.filter(function (atleta) {
+      return atleta.sport == deporte;
+    });
+  }
+  return filterPerSport;
 }
-/*export function searchByGender(Genero){
-  let filterPerGender=data.athletes.filter(function(gender) {
-  return gender.gender== Genero;
-})
-return filterPerGender
-}*/
+export function searchByGender(dataX, genero, buscarTodo) {
+  let filterPerGender;
+  if (buscarTodo) {
+    filterPerGender = data.athletes.filter(function (atleta) {
+      return atleta.gender == genero;
+    });
+  } else {
+    filterPerGender = dataX.filter(function (atleta) {
+      return atleta.gender == genero;
+    });
+  }
+  return filterPerGender;
+}
 
-export function searchByMedal(Medallas){
-  let filterPerMedal=data.athletes.filter(function(medal) {
-  return medal.medal == Medallas;
-})
-return filterPerMedal
+export function searchByMedal(dataX, medalla, buscarTodo) {
+  let filterPerMedal;
+  if (buscarTodo) {
+    filterPerMedal = data.athletes.filter(function (atleta) {
+      return atleta.medal == medalla;
+    });
+  } else {
+    filterPerMedal = dataX.filter(function (atleta) {
+      return atleta.medal == medalla;
+    });
+  }
+  return filterPerMedal;
 }
--- src/main.js
+++ src/main.js
@@ -1,48 +1,97 @@
-import { searchByTeam} from './data.js';
+import {searchByTeam} from './data.js';
 import{searchBySport} from './data.js';
-//import{searchByGender}from'./data.js';
+import{searchByGender}from'./data.js';
 import{searchByMedal} from'./data.js';
 //import athletes from './data/athletes/athletes.js';
 //import data from './data/athletes/athletes.js';
 
 /*let titulo = document.getElementById("olympics-title");
 */
+const firstPage= document.querySelector(".reseña");
 let divAthletes =  document.getElementById("athletes");
 let selectPais = document.getElementById("pais")
-let selectSport= document.getElementById("Deportes")
-//let selectGender=document.getElementById("Género")
+let selectSport= document.getElementById("deportes")
+let selectGender=document.getElementById("genero")
+let selectMedal = document.getElementById("medallas")
 let pagination = document.getElementById("pagination");
-let selectMedal = document.getElementById("Medallas")
-let avatar = 9;
+let avatar = 12;
 let currentPage = 1;
-selectPais.addEventListener("change",function(e){
+console.log("firstpage" + firstPage);
+showSuccess(false);
+document.querySelector(".button").addEventListener("click", reset);
+
+selectPais.addEventListener("change",function(){
+    filtrar();
+    showSuccess(true);
+ })
+
+ selectSport.addEventListener("change",function(){
+    filtrar();
+    showSuccess(true);
+ })
+
+ selectGender.addEventListener("change",function(){
+    filtrar();
+    showSuccess(true);
+ })
+ 
+ selectMedal.addEventListener("change",function(){
+    filtrar();
+    showSuccess(true);
+ })
+
+ function filtrar(){
+    let items = [];//determinamos el array vacio para que lo ocupe dataX cuando se hace una busqueda previa
+    let pais = selectPais.value;
+    let deporte = selectSport.value;
+    let genero = selectGender.value;
+    let medal = selectMedal.value;
     divAthletes.innerHTML = "";
     pagination.innerHTML = "";
-    const items = searchByTeam(e.target.value);
+    //console.log("Deporte: " + deporte);
+    //console.log("pais: " + pais);
+    //console.log("genero: " + genero);
+    //console.log("medalla: " + medal);
+
+    if(pais != "Selecciona un pais"){
+       if(items.length == 0){
+           items = searchByTeam(items,pais, true);
+       }
+       items = searchByTeam(items,pais, false);
+       //console.log(items);
+    }
+
+    if(deporte != "Selecciona una Disciplina"){
+        if(items.length == 0){
+            items=searchBySport(items, deporte, true);
+        }
+        items = searchBySport(items, deporte, false);
+        
+    }
+
+    if (genero != "Selecciona el género"){
+        if (items.length== 0){
+            items = searchByGender(items,genero, true);
+        }
+        items= searchByGender(items, genero, false);
+        
+    }
+
+    if(medal != "Selecciona una medalla" ){
+        //console.log("traigo esta medalla: "+medal);
+        if (items.length == 0){
+            items = searchByMedal(items, medal, true);
+        }
+        items = searchByMedal(items, medal, false);
+    }
+    if(items.length == 0){
+        divAthletes.innerHTML= "Tu busqueda no obtuvo resultados. Realiza una nueva";
+    }
+        //console.log(items);
     setupPagination(items, pagination, avatar );
     DisplayList( items, divAthletes, avatar, currentPage);
-});
-selectSport.addEventListener("change",function(e){
-    divAthletes.innerHTML = "";
-    pagination.innerHTML = "";
-    const items = searchBySport(e.target.value);
-    setupPagination(items, pagination, avatar );
-    DisplayList( items, divAthletes, avatar, currentPage);
-});
-selectMedal.addEventListener("change",function(e){
-    divAthletes.innerHTML = "";
-    pagination.innerHTML = "";
-    const items = searchByMedal(e.target.value);
-    setupPagination(items, pagination, avatar );
-    DisplayList( items, divAthletes, avatar, currentPage);
-});
-/*selectGender.addEventListener("change",function(e){
-    divAthletes.innerHTML = "";
-    pagination.innerHTML = "";
-    const items = searchByGender(e.target.value);
-    setupPagination(items, pagination, avatar );
-    DisplayList( items, divAthletes, avatar, currentPage);
-});*/
+ }
+
 function setupPagination (items, wrapper,avatarPerPage){
     wrapper.inderHtml="";
 
@@ -56,7 +105,13 @@ function setupPagination (items, wrapper,avatarPerPage){
     //console.log("Esto tiene el wrapper");
     //console.log(wrapper);
 }
-
+function showSuccess(esconder) {
+    firstPage.hidden = esconder;
+  }
+  
+function reset(){
+    
+}
 function DisplayList (items, divAthletes, avatarPerPage, page) {
     page--;
     let loopStart = avatarPerPage * page;
